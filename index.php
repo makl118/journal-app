fb9cf02657c8bddeb921e0bf691b006b5e3e4dad
Marcus Klausen <marcusklausen@gmail.com>
2021-01-11 21:57:55 +0100

    Added bootstrap, added journal entry form and button

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+    </div>
+
+    <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-5">
+        <h1>Journal App</h1>
+        <div class="row mb-3">
+            <div class="col-10">
+                    <label for="exampleFormControlTextarea1" class="form-label">What's on your mind?</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="col-2 d-flex flex-column">
+            <button type="button" class="btn btn-primary mt-auto">Add</button>
+            </div>
+        </div>
+        
+        
+        <h2>Earlier entries</h2>
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+    
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
