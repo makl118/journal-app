4377bd97c323b6ebdb6ed0315b33af2f8518c59f
Marcus Klausen <marcusklausen@gmail.com>
2021-01-15 02:21:07 +0100

    updated markup for journal page, introduced bug which removes side columns

--- index.php
+++ index.php
@@ -1,44 +1,56 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-    </div>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <div class="container">
 
     <div class="row">
-    <div class="col">
-      1 of 3
-    </div>
-    <div class="col-5">
-        <h1>Journal App</h1>
-        <div class="row mb-3">
-            <div class="col-10">
-                    <label for="exampleFormControlTextarea1" class="form-label">What's on your mind?</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-            <div class="col-2 d-flex flex-column">
+      <div class="col d-none d-md-inline">
+      </div>
+      <div class="col-*">
+        <h1 class="display-2 mt-3">Journal App</h1>
+        <div class="row mb-5">
+          <div class="col-10">
+            <label for="exampleFormControlTextarea1" class="form-label">What's on your mind?</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <div class="col-2 d-flex flex-column">
             <button type="button" class="btn btn-primary mt-auto">Add</button>
-            </div>
+          </div>
+        </div><!-- row -->
+
+        <div class="row">
+          <h1 class="display-5 mb-3">Earlier entries</h1>
         </div>
-        
-        
-        <h2>Earlier entries</h2>
+        <div class="row mb-3">
+          <div class="card mb-3">
+            <div class="row g-0">
+              <div class="card-body">
+                <h5 class="card-title">Today I went to the mooon...</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi purus, venenatis eget rhoncus ut, consequat id nisl. Aliquam at justo nec purus euismod feugiat ut in erat. Ut elementum felis sit amet convallis interdum. Etiam in tellus dapibus, mattis nulla pulvinar, viverra ligula. Praesent in varius magna, sed porttitor ante. Donec ullamcorper purus sed nisi consectetur, eu sollicitudin dui lacinia.</p>
+                <p class="card-text"><small class="text-muted">15th of December 2019</small></p>
+              </div>
+            </div>
+          </div>
+        </div><!-- row -->
+      </div><!-- col -->
+      <div class="col d-none d-md-inline">
+      </div>
     </div>
-    <div class="col">
-      3 of 3
-    </div>
-  </div>
-    
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
